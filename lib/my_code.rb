a787a35bb8c1d52943b3a3accfeae3143dfc3f26
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-19 20:24:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+
+# do => end 
+# [1,2,3].map do |num|
+# num + 1 
+# end
+
+# [1,2,3].map {|num| nim + 1}
+
+#yield accepts an argument 
+
+# def add_numbers(num1, num2)
+# total = num1 + num2
+# yield(total)
+# total 
+# end 
+
+# add_numbers(1,2) do |t| 
+# puts "The total is #{t}"
+# end 
+
