a812d4b98eafb7b22ecb15033b6323be0c351e5b
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-19 20:48:16 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,46 @@
 # Your Code Here
+
+# do => end 
+# [1,2,3].map do |num|
+# num + 1 
+# end
+
+# [1,2,3].map {|num| nim + 1}
+
+#yield accepts an argument 
+
+# def add_numbers(num1, num2)
+# total = num1 + num2
+# yield(total)
+# total 
+# end 
+
+# add_numbers(1,2) do |t| 
+# puts "The total is #{t}"
+# end 
+
+def map (array)
+  #map returns a new array of manipulated elements
+  new = [] 
+  i = 0 
+  while i < array.length
+    new.push(yield(array[i]))
+    i +=1 
+  end
+  new 
+end
+
+def reduce (array, sv=nil)
+  if sv 
+    sum = sv 
+    i = 0 
+  else 
+    sum = array [0]
+    i = 1 
+  end
+  while i < array.length
+    sum = yield(sum, array[i])
+    i +=1 
+  end
+  sum 
+end 
